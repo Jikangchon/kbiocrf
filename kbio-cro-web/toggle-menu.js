8e8cc46f2675887e6751fd2c3c65c8850594a371
JK Chon <77332173+Jikangchon@users.noreply.github.com>
2023-02-13 16:28:05 +0900

    Mobile-Responsive-UI2

---
+++ kbio-cro-web/toggle-menu.js
@@ -0,0 +1,9 @@
+function sideMenu() {
+    let menu = document.querySelector(".toggle-menu");
+    menu.classList.toggle("active"); 
+}
+
+function sideMenu() {
+    let side_menu = document.querySelector(".side_menu");
+    side_menu.classList.toggle("active");
+}
